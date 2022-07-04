84f5a69fbac286265d6ceda361427e2f4672d9c6
abbasoov <ismayiliib@code.edu.az>
2022-07-04 15:31:04 +0400

    added homework

---
+++ assets/JS/app.js
@@ -0,0 +1,12 @@
+function AgeCheck(){
+  var age = prompt('Please, Enter Your Age', 1)
+  if (age<=18) {
+
+alert('You are not elder enough')
+    
+  }
+  
+  else{
+    alert('Registered successfully')
+  }
+}
