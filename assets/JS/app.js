d9974f019468d24fcb023bd1b3aa5f59ff41a590
abbasoov <ismayiliib@code.edu.az>
2022-07-04 15:41:00 +0400

    modified

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,11 +1,11 @@
 function AgeCheck(){
   var age = prompt('Please, Enter Your Age', 1)
-  if (age<=18) {
+  if (age<18) {
 
 alert('You are not elder enough')
     
   }
-  
+
   else{
     alert('Registered successfully')
   }
